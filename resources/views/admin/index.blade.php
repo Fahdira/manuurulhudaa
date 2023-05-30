1fed8c730cc4f0a9ec4ba33e29acc90c74a63ba7
Muhammad Fahdira <134904783+kerbulll@users.noreply.github.com>
2023-05-30 10:14:35 +0700

    Update index.blade.php

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -20,19 +20,77 @@
     </div>
 </div>
 
-
-
-@endsection
-
-@section('extra-content')
-    <!-- start here -->
-@endsection
-
-@section('extra-js')
-    <!-- start here -->
 @endsection
 
 @section('script')
-    <!-- start here -->
+    <!-- Bootstrap core JavaScript-->
+    <script src="https://penerimaan.darussalamciamis.or.id/sbadmin/vendor/jquery/jquery.min.js"></script>
+    <script src="https://penerimaan.darussalamciamis.or.id/sbadmin/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="https://penerimaan.darussalamciamis.or.id/sbadmin/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="https://penerimaan.darussalamciamis.or.id/sbadmin/js/sb-admin-2.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+    <script>
+        //message with toastr
+
+        var $loading = $('#ajaxSpinnerContainer').hide();
+        $(document)
+            .ajaxStart(function() {
+                $loading.show();
+            })
+            .ajaxStop(function() {
+                $loading.hide();
+            });
+
+        $(function() {
+            $('.btn-group-fab').on('click', '.btn', function() {
+                window.location.href = 'https://wa.me/+6281321551318';
+            });
+            $('has-tooltip').tooltip();
+        });
+    </script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js">
+    </script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.3/js/dataTables.bootstrap4.min.js">
+    </script>
+    <script type="text/javascript" charset="utf8"
+        src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/responsive/2.2.9/js/responsive.bootstrap4.min.js">
+    </script>
+    <script>
+        $(document).ready(function() {
+            $('#prestasi-table').DataTable({
+                paging: false,
+                searching: false,
+                ordering: false,
+                info: false,
+                responsive: true,
+                ajax: "https://penerimaan.darussalamciamis.or.id/mendaftar/getPrestasi",
+                columns: [{
+                        data: 'nama',
+                        name: 'nama'
+                    },
+                    {
+                        data: 'peringkat',
+                        name: 'peringkat'
+                    },
+                    {
+                        data: 'tingkat',
+                        name: 'tingkat'
+                    },
+                    {
+                        data: 'tahun',
+                        name: 'tahun'
+                    }
+                ],
+            });
+        });
+    </script>
 @endsection
+
+
+
 
